836a033dd08c05ad2008f777a490259f6e6ecfbf
POMXARK <1997pom@gmail.com>
2022-03-06 11:46:35 +1000

    start +++

--- test/PostService.js
+++ test/PostService.js
@@ -1,16 +1,9 @@
-import Post from "./Post.js";
+import Post from "./Post";
 
-class PostController{
-    async create(req, res){
-        try{
-            console.log(req.body);
-            const {author, title, content, picture} = req.body
-            const post = await Post.create({author, title, content, picture})
-            //res.status(200).json(`Сервер работает`)
-            res.json(post)
-        } catch (e){
-            res.status(500).json(e)
-        }
+class PostService{
+    async create(post){
+        const createdPost = await  Post.create(post);
+        return createdPost;
     }
     async getAll(req, res){
         try{
@@ -58,4 +51,4 @@ class PostController{
     }
 }
 
-export default new PostController();
+export default new PostService();
